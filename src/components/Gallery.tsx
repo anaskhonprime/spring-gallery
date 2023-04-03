dca01250030724d7f93b409f3b143280ce9ebab4
anaskhonprime <smlife202@gmail.com>
2023-04-03 21:31:25 +0900

    Gallery Filter Functions Updated

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,29 +4,52 @@ import { springImages, buttons } from "../data/spring";
 
 function Gallery() {
 
-    const [gallery, setGallery] = useState(0);
+    interface SpringImage {
+        category: string;
+        url: string;
+    }
 
-    const getImages = () => {
+    const springImages: SpringImage[] = [
+        {
+            category: "flowers",
+            url:
+                "https://i.guim.co.uk/img/media/d5b563bef60c06a22dc65a18c391f63ff42549e6/0_205_4288_2573/master/4288.jpg?width=1200&quality=85&auto=format&fit=max&s=f69dafe4bb0bc828c5e07298240f1d3a",
+        },
+        {
+            category: "flowers",
+            url:
+                "https://hips.hearstapps.com/hmg-prod/images/spring-meadow-royalty-free-image-1579125133.jpg",
+        },
+        {
+            category: "flowers",
+            url: "https://glamadelaide.com.au/wp-content/uploads/2020/09/s-spring-flowers.jpg",
+        },
+    ];
+
+    const [gallery, setGallery] = useState<SpringImage[]>([]);
+
+    function getImages(): SpringImage[] {
         const imageList = springImages;
         return imageList;
     }
 
     useEffect(() => {
-        setGallery(getImages);
+        setGallery(getImages());
     }, []);
 
-    const filterImages = (imageType) => {
-        let singleImage:string = getImages().filter(type => type.category === imageType);
+    function filterImages(imageType: string): SpringImage[] {
+        let singleImage: SpringImage[] = getImages().filter(
+            (type) => type.category === imageType
+        );
         return singleImage;
     }
 
-    const handleFilter = (e) => {
+    function handleFilter(e: React.ChangeEvent<HTMLSelectElement>) {
         let result = e.target.value;
         result !== "all"
             ? setGallery(filterImages(result))
             : setGallery(getImages());
     }
-
     return (
         <section>
             <div className="row g-0">
@@ -44,10 +67,10 @@ function Gallery() {
                                         className="filter-button"
                                         key={index}
                                         value={type.value}
-                                        onClick={handleFilter}
+                                        onclick={handleFilter}
                                         type="button"
                                     >
-                                        type.name
+                                        {type.name}
                                     </button>
                                 </>
                             ))}
