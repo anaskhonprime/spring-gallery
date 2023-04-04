6ada5817ab5008d4b9fb905755f60d358ddb91ab
anaskhonprime <smlife202@gmail.com>
2023-04-04 15:51:46 +0900

    Completed Handel Filter, Mobile UI Componenst

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -9,23 +9,6 @@ function Gallery() {
         url: string;
     }
 
-    const springImages: SpringImage[] = [
-        {
-            category: "flowers",
-            url:
-                "https://i.guim.co.uk/img/media/d5b563bef60c06a22dc65a18c391f63ff42549e6/0_205_4288_2573/master/4288.jpg?width=1200&quality=85&auto=format&fit=max&s=f69dafe4bb0bc828c5e07298240f1d3a",
-        },
-        {
-            category: "flowers",
-            url:
-                "https://hips.hearstapps.com/hmg-prod/images/spring-meadow-royalty-free-image-1579125133.jpg",
-        },
-        {
-            category: "flowers",
-            url: "https://glamadelaide.com.au/wp-content/uploads/2020/09/s-spring-flowers.jpg",
-        },
-    ];
-
     const [gallery, setGallery] = useState<SpringImage[]>([]);
 
     function getImages(): SpringImage[] {
@@ -44,8 +27,8 @@ function Gallery() {
         return singleImage;
     }
 
-    function handleFilter(e: React.ChangeEvent<HTMLSelectElement>) {
-        let result = e.target.value;
+    function handleFilter(event: any) {
+        let result = event.target.value;
         result !== "all"
             ? setGallery(filterImages(result))
             : setGallery(getImages());
@@ -56,44 +39,61 @@ function Gallery() {
                 <div className="col-lg-3">
                     <div className="fixed-part">
                         <h2>Spring Gallery</h2>
-                        <p>
-                            The Spring season is a time of year when the weather starts to change.</p>
-                        <hr />
-                        <h2>Discover</h2>
+                          <hr/>
                         {buttons &&
                             buttons.map((type, index) => (
-                                <>
+                                <div className="buttons-box">
                                     <button
                                         className="filter-button"
                                         key={index}
                                         value={type.value}
-                                        onclick={handleFilter}
+                                        onClick={handleFilter}
                                         type="button"
                                     >
                                         {type.name}
                                     </button>
-                                </>
-                            ))}
-                    </div>
-
-                </div>
-                <div className="col-lg-9">
-                    <div className="center">
-                        <div className="gallery-items">
-                            {springImages.map((key, a) => (
-                                <div key={a} className="spring-img">
-                                    <img src={springImages[a].url} alt="images" />
-                                    <div className="tag">
-                                        <h6 className="pinky">#spring</h6>
-                                        <h6>#{springImages[a].category}</h6>
-                                    </div>
                                 </div>
                             ))}
-
+                        <hr />
+                        <div className="check">
+                            {buttons &&
+                                buttons.map((type, index) => (
+                                    <div className="row g-0 box">
+                                        <div className="col-lg-1">
+                                            <button
+                                                className="check-button"
+                                                key={index}
+                                                value={type.value}
+                                                onClick={handleFilter}
+                                                type="button"
+                                            />
+                                        </div>
+                                        <div className="col-lg-11">
+                                            <div className="check-p">{type.name}</div>
+                                        </div>
+                                    </div>
+                                ))}
                         </div>
                     </div>
                 </div>
-
+                <div className="col-lg-9">
+                    <div className="center">
+                        <p>Total Images : {gallery.length}</p>
+                        <hr />
+                        <div className="gallery-items">
+                            {gallery &&
+                                gallery.map((type, a) => (
+                                    <div key={a} className="spring-img">
+                                        <img src={gallery[a].url} alt="images" />
+                                        <div className="tag">
+                                            <h6 className="pinky">#spring</h6>
+                                            <h6>#{gallery[a].category}</h6>
+                                        </div>
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
